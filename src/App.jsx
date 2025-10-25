4630da41c5ecbb94439090204684da5fdf2e92cb
G-ESWARBHAI <eswargeddam1432@gmail.com>
2025-10-25 10:49:28 +0530

    Update App.jsx and Homepage.jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar'
-import Hero from './components/hero'
-import Services from './components/services'
-import BookConsultation from './components/bookConsultation'
-import BookAppointment from './components/bookAppointment'
 import Footer from './components/footer'
-import AboutSasha from './components/about'
-import Explore from './components/Explore'
+import Homepage from './pages/Homepage'
+
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <Hero />
-      <AboutSasha />
-      <Services />
-      <BookConsultation />
-      <Explore />
-      <BookAppointment />
+      <Homepage />
       <Footer />
     </div>
   )
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Hero from '../components/hero'
+import Services from '../components/services'
+import BookConsultation from '../components/bookConsultation'
+import BookAppointment from '../components/bookAppointment'
+import AboutSasha from '../components/about'
+import Explore from '../components/Explore'
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="min-h-screen">
+      <Hero />
+      <AboutSasha />
+      <Services />
+      <BookConsultation />
+      <Explore />
+      <BookAppointment />
     </div>
   )
 }
